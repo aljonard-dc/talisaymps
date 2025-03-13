cf91fd8c1c84fae8b82993825e9f8dfa9afda0fe
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-03-13 16:38:37 +0800

    add create function

---
+++ app/api/actions.ts
@@ -0,0 +1,75 @@
+'use server'
+
+import pool from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+// Add Case Record
+export async function createCaseRecord(formData: FormData) {
+  const caseFileNo = formData.get("caseFileNo");
+  const respondent = formData.get("respondent");
+  const caseTitle = formData.get("caseTitle");
+  const dateFiled = formData.get("dateFiled");
+  const criminalCaseNo = formData.get("criminalCaseNo");
+  const investigator = formData.get("investigator");
+  const complainants = formData.get("complainant");
+  const remarks = formData.get("remarks");
+
+  if (
+    !caseFileNo ||
+    !respondent ||
+    !caseTitle ||
+    !dateFiled ||
+    !criminalCaseNo ||
+    !investigator ||
+    !complainants ||
+    !remarks
+  ) {
+    throw new Error("All fields are required");
+  }
+
+  await pool.query(
+    `INSERT INTO "caserecords" 
+    ("caseFileNo", respondent, "caseTitle", "dateFiled", "criminalCaseNo", "investigatorOnCase", complainant, remarks) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+    [caseFileNo, respondent, caseTitle, dateFiled, criminalCaseNo, investigator, complainants, remarks]
+  );
+  revalidatePath("/case-records");
+}
+
+// Update Case Record
+export async function updateCaseRecord(formData: FormData) {
+  const id = formData.get("id");
+  const caseFileNo = formData.get("caseFileNo");
+  const respondent = formData.get("respondent");
+  const caseTitle = formData.get("caseTitle");
+  const dateFiled = formData.get("dateFiled");
+  const criminalCaseNo = formData.get("criminalCaseNo");
+  const investigator = formData.get("investigator");
+  const complainants = formData.get("complainant");
+  const remarks = formData.get("remarks");
+
+  if (!id) {
+    throw new Error("Invalid ID");
+  }
+
+  await pool.query(
+    `UPDATE "caserecords" SET 
+    "caseFileNo" = $1, respondent = $2, "caseTitle" = $3, "dateFiled" = $4, 
+    "criminalCaseNo" = $5, "investigatorOnCase" = $6, complainant = $7, remarks = $8 
+    WHERE id = $9`,
+    [caseFileNo, respondent, caseTitle, dateFiled, criminalCaseNo, investigator, complainants, remarks, id]
+  );
+  revalidatePath("/case-records");
+}
+
+// Delete Case Record
+export async function deleteCaseRecord(formData: FormData) {
+  const id = formData.get("id");
+
+  if (!id) {
+    throw new Error("Invalid ID");
+  }
+
+  await pool.query('DELETE FROM "caserecords" WHERE id = $1', [id]);
+  revalidatePath("/case-records");
+}
